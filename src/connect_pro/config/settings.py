0df65ffb65a6d5ac9c104796ef577d81b9210d27
Ruslan Yermakov <ruslan.yermakov@personal.com>
2025-01-10 17:57:20 +0100

    Add tavily key to settings

--- src/connect_pro/config/settings.py
+++ src/connect_pro/config/settings.py
@@ -1,23 +1,26 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 
+
 class Settings(BaseSettings):
     """Application settings."""
+
     PROXYCURL_API_KEY: str
     OPENAI_API_KEY: str
-    OPENAI_MODEL_NAME: str
+    TAVILY_API_KEY: str
 
     # Other settings with defaults
-    # MOCK_ENABLED: bool = False
-    # TIMEOUT: int = 10
-
+    OPENAI_MODEL_NAME: str = "gpt-4o-mini"
+    
     # Configure .env file loading
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
 
 @lru_cache()
 def get_settings() -> Settings:
     """Get cached settings."""
     return Settings()
+
 
 # Create a settings instance to import
 settings = get_settings()
